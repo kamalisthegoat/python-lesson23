7b22e6bc1f559c877f46378c235eef0f5d773e87
kamalisthegoat <manjusundaram1983@gmail.com>
2026-06-06 20:30:29 +0530

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,12 @@
+#python program to demonstarte
+#exit()
+for i in range(10):
+    # if the value of i becomes
+    #5 then the program if forced to exit
+    if i == 5:
+
+        #print the exit message
+
+        print(exit)
+        exit()
+    print(i)    
